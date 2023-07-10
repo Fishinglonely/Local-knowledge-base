99c92f7c9df786adf517f7cde61acec5dfd32661
Fishinglonely <1023145583@qq.com>
2023-07-10 18:23:30 +0800

    Update app_test.py
    修改账号信息

--- app_test.py
+++ app_test.py
@@ -8,14 +8,14 @@ from pathlib import Path
 
 # in01-70ff1fe5d9bc5a0.aws-us-west-2.vectordb.zillizcloud.com
 
-HOST = "in03-dc41c1bb9c9c889.api.gcp-us-west1.zillizcloud.com"  # Host in Zilliz Cloud endpoint
+HOST = "xxx"  # Host in Zilliz Cloud endpoint
 PORT = 443  # Port in Zilliz Cloud endpoint
-TOKEN = "80327f415c8bbd61d3b49e1e10ea4e989754b26659bb5b729b61111cf8a11410a477e1582c9187b6f96a5eb43d7715e5a63c445b"
-# TOKEN = "1023145583@qq.com:082930aA~"
-USER = "1023145583@qq.com"  # Username for the cluster
-PASSWORD = "082930aA~"  # Password that goes with the user
+TOKEN = "xxx"
+# TOKEN = "xxx"
+USER = "xxx"  # Username for the cluster
+PASSWORD = "xxx"  # Password that goes with the user
 
-environ["OPENAI_API_KEY"] = "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"  # OpenAI API Key
+environ["OPENAI_API_KEY"] = "sk-xxx"  # OpenAI API Key
 
 markdownreader = MarkdownReader.MarkdownReader()
 # Grab all markdown files and convert them using the reader
